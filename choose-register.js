a218fecc62283b7924dcb3e1eeb0a8344e4978d2
Stefany Carlos <stefanycarlos.12345@gmail.com>
2024-10-31 10:00:58 -0300

    commit
    commit

---
+++ choose-register.js
@@ -0,0 +1,31 @@
+const activeClass = "active";
+
+function animateLinks(navLinks) {
+  navLinks.forEach((link, index) => {
+    link.style.animation
+      ? (link.style.animation = "")
+      : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+  });
+}
+
+function handleClick(mobileMenu, navList, navLinks) {
+  navList.classList.toggle(activeClass);
+  mobileMenu.classList.toggle(activeClass);
+  animateLinks(navLinks);
+}
+
+function addClickEvent(mobileMenu, navList, navLinks) {
+  mobileMenu.addEventListener("click", () => handleClick(mobileMenu, navList, navLinks));
+}
+
+function initMobileNavbar(mobileMenuSelector, navListSelector, navLinksSelector) {
+  const mobileMenu = document.querySelector(mobileMenuSelector);
+  const navList = document.querySelector(navListSelector);
+  const navLinks = document.querySelectorAll(navLinksSelector);
+
+  if (mobileMenu) {
+    addClickEvent(mobileMenu, navList, navLinks);
+  }
+}
+
+initMobileNavbar(".mobile-menu", ".nav-list", ".nav-list li");
